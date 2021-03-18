e8d160c63e5da3d3b2759758bdbc6cd76c633822
Asım MISIRLI <asimmisirli@hotmail.com>
2021-03-18 17:19:15 +0100

    Update pemconvert.php

--- pemconvert.php
+++ pemconvert.php
@@ -4,14 +4,14 @@
 function analaysis($cert){
 
     $data2=openssl_x509_parse( openssl_x509_read($cert));
-    //I get information from data2(data2 is array)
-
+    //When we converted. Data2 will be array.
+    //if you want look all text in pem file. you should  write this line.  print_r($data2);
     echo "hostname: ".$data2["subject"]["CN"]."\n";
     echo $data2["issuer"]["O"];
     echo "\n"."Valid From time: ".date('Y-m-d H:i:s',$data2['validFrom_time_t']);
     echo "\n"."Valid To time: ".date('Y-m-d H:i:s',$data2['validTo_time_t']);;
 
-// You could check end of SSL date
+// This line could check end of SSL date
     if (date("Y-m-d H:i:s") <= date('Y-m-d H:i:s',$data2['validTo_time_t'])){
         echo "\n"."Not expired";
     }else{
@@ -20,7 +20,7 @@ function analaysis($cert){
 
 }
 
-//this line can read pem file.
+//this line could read pem file
 $fp = fopen('asimmisirli-com.pem','r');
 $cert = fread($fp,8192);
 fclose($fp);
